e252bb44ad67b69b99b4e984f274d4cd4a4e7140
RishabhTibrewal <96077545+RishabhTibrewal@users.noreply.github.com>
2024-02-19 10:24:55 +0530

    Update NFT.sol

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
+pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract NFT is ERC721 {
 
-    constructor() ERC721("DAOMember", "DM") {}
+    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
 
     function mint() public {
         _mint(msg.sender, 1);
